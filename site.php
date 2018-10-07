a584c9b374d4d50e727dd1b52015f41d69f0e5bd
Jesus Silva <jesus-arturo@hotmail.com>
2018-10-07 23:03:43 +0100

    categorias VS products

--- site.php
+++ site.php
@@ -1,16 +1,34 @@
 <?php 
 
-	use Hcode\Page;
-	use Hcode\Model\Product;
+	use \Hcode\Page;
+	use \Hcode\Model\Product;
+	use \Hcode\Model\Category;
+
 
 	$app->get('/', function() {
 
 		$products = Product::listAll();
-    
+	    
+		$page = new Page();
+
+		$page->setTpl("index", [
+			'products'=>Product::checkList($products)
+		]); 
+	});
+
+
+	$app->get("/categories/:idcategory", function($idcategory){
+
+	$category = new Category();
+
+	$category->get((int)$idcategory);
+
 	$page = new Page();
-	$page->setTpl("index", [
-		'products'=>Product::checkList($products)
-	]); 
+
+	$page->setTpl("category", [
+		'category'=>$category->getValues(),
+		'products'=>Product::checkList($category->getProducts()),
+	]);
 
 });
 
